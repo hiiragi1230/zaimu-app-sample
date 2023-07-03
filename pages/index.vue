fdc110fb237cc8f8423001b5c3ef76d14b11aea8
sumna7 <104060670+natsu777@users.noreply.github.com>
2023-07-03 13:38:47 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="container">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+      <div class="container-fluid">
+        <a class="navbar-brand sticky-top p-3 font-weight-bold navbar-brand h1" href="#">財務会計システム</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">各種メニュー</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">マイページ</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">アカウント情報</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+  </template>
+
