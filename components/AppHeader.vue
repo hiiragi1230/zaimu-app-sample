cacd928a5c606bdb7a60a2017d85ab1fe4788d06
sumna7 <104060670+natsu777@users.noreply.github.com>
2023-07-03 14:47:59 +0900

    nav and body

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,3 +1,26 @@
 <template>
-
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div class="container-fluid">
+        <h1 class="navbar-brand sticky-top p-3 font-weight-bold navbar-brand h1" href="#">財務会計システム</h1>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">各種メニュー</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">マイページ</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">アカウント情報</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,45 @@
 <template>
-  <header class="container">
-  <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-      <div class="container-fluid">
-        <a class="navbar-brand sticky-top p-3 font-weight-bold navbar-brand h1" href="#">財務会計システム</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">各種メニュー</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">マイページ</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">アカウント情報</a>
-            </li>
-          </ul>
-        </div>
+<div class="container">
+  <div class="row mx-auto p-4">
+    <div class="col-sm-6">
+      <div class="card my-3" style="width: 18rem;">
+      <div class="card-header">
+      会社情報
       </div>
-    </nav>
-  </header>
-  </template>
-
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">
+          <a class="nav-link" href="#">会社管理</a>
+        </li>
+        <li class="list-group-item">
+          <a class="nav-link" href="#">ソフト管理</a>
+        </li>
+        <li class="list-group-item">
+          <a class="nav-link" href="#">メンバー管理</a>
+        </li>
+      </ul>
+      </div>
+    </div>
+    <div class="col-sm-6">
+      <div class="card my-3" style="width: 18rem;">
+      <div class="card-header">
+      アカウント情報
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">
+          <a class="nav-link" href="#">ユーザー情報</a>
+        </li>
+        <li class="list-group-item">
+          <a class="nav-link" href="#">パスワード変更</a>
+        </li>
+        <li class="list-group-item">
+          <a class="nav-link" href="#">メールアドレス変更</a>
+        </li>
+        <li class="list-group-item">
+          <a class="nav-link" href="#">アカウント解約</a>
+        </li>
+      </ul>
+      </div>
+    </div>
+    </div>
+  </div>
+</template>
