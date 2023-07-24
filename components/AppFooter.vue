65b1181fd78c2f2ff16776a9fe255e625fd0b717
sumna7 <104060670+natsu777@users.noreply.github.com>
2023-07-24 13:50:24 +0900

    a

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,10 +1,10 @@
 <template>
-<div class="mt-4 py-3 bg-light">
+<!-- <div class="mt-4 py-3 bg-light">
     <div class="text-end">
       <a href="">Privacy Policy</a> <a href="">Terms</a>
     </div>
     <div class="text-center">
       <span class="text-muted">Demo co., &copy; All Right Reserved.</span>
     </div>
-    </div>
+    </div> -->
 </template>
